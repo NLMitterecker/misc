d931731aed5ade3e672ba997310038b762a79357
NLMitterecker <a0500315@unet.univie.ac.at>
2019-09-04 15:21:27 +0200

    further housekeeping

--- DBImportHelper/tests/test_DataImport.py
+++ DBImportHelper/tests/test_DataImport.py
@@ -6,17 +6,11 @@ __copyright__ = "Copyright 2019 by Daniel Mitterecker"
 __licence__ = "GNU General Public License version 3 or later (GPLv3+)"
 
 import os, sys
-config = {
-    "tests_dir": "tests/",
-    "src_dir": "src/"
-}
 
-script_path=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-full_src_path = os.path.dirname(os.path.join(script_path, config['src_dir']))
-full_tests_path = os.path.dirname(os.path.join(script_path, config['tests_dir']))
+script_path=os.path.dirname(os.path.dirname(__file__))
 
+full_src_path = os.path.dirname(os.path.join(script_path, "src/"))
 sys.path.append(full_src_path)
-sys.path.append(full_tests_path)
 
 import unittest
 import DataImport as DI
