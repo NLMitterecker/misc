53e8470f1bf4cc193069462488f5875fdd77568e
NLMitterecker <a00500315@unet.univie.ac.at>
2019-09-05 14:56:59 +0200

    create first testtable via sqlalchemy in pgsql

--- SQLAlchemy_Tests/pg_test.py
+++ SQLAlchemy_Tests/pg_test.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
-from sqlalchemy import create_engine
+from datetime import datetime
+from sqlalchemy import (MetaData, create_engine, Table, Column, 
+                        Integer, Numeric, String, ForeignKey, DateTime)
+
+metadata = MetaData()
+
+users = Table('cookies', metadata,
+        Column('cookie_id', Integer(), primary_key=True),
+        Column('cookie_name', String(50), index=True, nullable=False),
+        Column('cost', Numeric(12, 2)),
+        Column('added', DateTime(), default=datetime.now)
+)
+
 engine = create_engine('postgresql+psycopg2://learn_sqlalchemy:learn123@localhost:'\
                                                             '5432/learn_sqlalchemy')
+
+metadata.create_all(engine)
