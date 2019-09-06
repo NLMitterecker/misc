91dac55e067e5dfc4ed69c5ad5df8871a2dbbf24
NLMitterecker <a00500315@unet.univie.ac.at>
2019-09-06 09:38:13 +0200

    added select statement for testing pg bounce

--- SQLAlchemy_Tests/pg_test.py
+++ SQLAlchemy_Tests/pg_test.py
@@ -3,20 +3,37 @@
 import time
 from datetime import datetime
 from sqlalchemy import (MetaData, create_engine, Table, Column, 
-                        Integer, Numeric, String, ForeignKey, DateTime)
+                        Integer, Numeric, String, ForeignKey, DateTime,
+                        insert, select)
 
 metadata = MetaData()
 
-users = Table('cookies', metadata,
+cookies = Table('cookies', metadata,
         Column('cookie_id', Integer(), primary_key=True),
         Column('cookie_name', String(50), index=True, nullable=False),
         Column('cost', Numeric(12, 2)),
         Column('added', DateTime(), default=datetime.now)
 )
 
+#ins = cookies.insert().values(
+#    cookie_name="chocolate chip",
+#    cost="1.5"
+#)
+
+# also possible ins = insert(cookies).values( ...
+#print(str(ins))
+
+# direct DB access
 #engine = create_engine('postgresql://learn_sqlalchemy:learn123@127.0.0.1:'\
 #                                                            '5432/learn_sqlalchemy')
 engine = create_engine('postgresql://learn_sqlalchemy:learn123@127.0.0.1:'\
                                                             '6543/learn_sqlalchemy')
-
 metadata.create_all(engine)
+
+connection = engine.connect()
+
+s = select([cookies])
+rp = connection.execute(s)
+result = rp.fetchall()
+
+connection.close()
